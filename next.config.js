2d9b2269b8490238a97c7a6b047d215e7cb3b10d
VoltNexis <voltedgebuilds@gmail.com>
2025-08-31 21:20:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // enables static export (no SSR)
+  basePath: "/watchandearn", // repo name
   images: {
+    unoptimized: true, // GitHub Pages doesn’t support next/image optimization
     domains: ['localhost', 'cloudflare.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
